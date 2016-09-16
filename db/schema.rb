63ded9cfe4abd867729de06d5c4cbf6a4ef35ef2
Antoine Reveau <antoinerev@gmail.com>
2016-09-16 12:07:24 +0200

    changed ingredient column name to not reserved word

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916094103) do
+ActiveRecord::Schema.define(version: 20160916100451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20160916094103) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "family"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
